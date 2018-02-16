2ba7e882dee846116120d3c1fbef40da219f9834
Even Modiguy <even.modiguy@kisio.org>
2018-02-16 18:08:47 +0100

    add network call for journey solutions and remove colors constants from Configuration class

--- navitiasdkux/src/main/java/org/kisio/navitiasdkui/journey/search/JourneySearchActivity.java
+++ navitiasdkux/src/main/java/org/kisio/navitiasdkui/journey/search/JourneySearchActivity.java
@@ -8,12 +8,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 
+import org.kisio.NavitiaSDK.NavitiaConfiguration;
+import org.kisio.NavitiaSDK.NavitiaSDK;
+import org.kisio.NavitiaSDK.apis.JourneysApi;
+import org.kisio.NavitiaSDK.invokers.ApiCallback;
+import org.kisio.NavitiaSDK.invokers.ApiException;
 import org.kisio.NavitiaSDK.models.Journey;
+import org.kisio.NavitiaSDK.models.Journeys;
 import org.kisio.NavitiaSDK.models.LinkSchema;
 import org.kisio.NavitiaSDK.models.Section;
 import org.kisio.navitiasdkui.R;
 import org.kisio.navitiasdkui.journey.ListModel;
 import org.kisio.navitiasdkui.journey.roadmap.RoadmapActivity;
+import org.kisio.navitiasdkui.util.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class JourneySearchActivity extends AppCompatActivity implements ResultAdapter.ClickListener {
     public final static String INTENT_PARAM = JourneySearchActivity.class.getSimpleName();
@@ -25,13 +36,13 @@ public class JourneySearchActivity extends AppCompatActivity implements ResultAd
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setBackgroundColor(fetchPrimaryColor());
+        toolbar.setBackgroundColor(Configuration.getColor(this, R.attr.colorPrimary));
         toolbar.setElevation(0);
         toolbar.setTitle(R.string.journeys);
 
         final JourneysRequest request = getIntent().getParcelableExtra(INTENT_PARAM);
         if (request != null) {
-            // Launch network request
+            launchRequest(request);
         }
     }
 
@@ -50,16 +61,6 @@ public class JourneySearchActivity extends AppCompatActivity implements ResultAd
         startActivity(intent);
     }
 
-    private int fetchPrimaryColor() {
-        TypedValue typedValue = new TypedValue();
-        TypedArray a = this.obtainStyledAttributes(typedValue.data, new int[] { R.attr.colorPrimary });
-        int color = a.getColor(0, 0);
-
-        a.recycle();
-
-        return color;
-    }
-
     private String getCarpoolDeepLink(Journey journey) {
         for (Section section : journey.getSections()) {
             if (section.getType().equalsIgnoreCase("street_network") && section.getMode().equalsIgnoreCase("ridesharing")) {
@@ -75,5 +76,70 @@ public class JourneySearchActivity extends AppCompatActivity implements ResultAd
             }
         }
         return null;
+    }
+
+    private void launchRequest(JourneysRequest request) {
+        try {
+            NavitiaSDK navitiaSDK = new NavitiaSDK(new NavitiaConfiguration(Configuration.getToken(this)));
+            final JourneysApi.JourneysRequestBuilder journeysRequestBuilder = navitiaSDK.journeysApi.newJourneysRequestBuilder();
+
+            journeysRequestBuilder
+                    .withFrom(request.getOriginId())
+                    .withTo(request.getDestinationId())
+                    .withDatetime(request.getDatetime())
+                    .withDatetimeRepresents(request.getDatetimeRepresents())
+                    .withAllowedId(request.getAllowedId())
+                    .withForbiddenUris(request.getForbiddenUris())
+                    .withFirstSectionMode(request.getFirstSectionModes())
+                    .withLastSectionMode(request.getLastSectionModes())
+                    .withCount(request.getCount())
+                    .withMinNbJourneys(request.getMinNbJourneys())
+                    .withMaxNbJourneys(request.getMaxNbJourneys())
+            ;
+
+            journeysRequestBuilder
+                    .get(new ApiCallback<Journeys>() {
+                        @Override
+                        public void onFailure(ApiException e, int statusCode, Map<String, List<String>> responseHeaders) {
+                            showError();
+                        }
+
+                        @Override
+                        public void onSuccess(Journeys result, int statusCode, Map<String, List<String>> responseHeaders) {
+                            if (result.getError() == null && result.getJourneys() != null) {
+                                List<Journey> classicJourneys = new ArrayList<>();
+                                List<Journey> carpoolingJourneys = new ArrayList<>();
+                                for (Journey journey : result.getJourneys()) {
+                                    if (journey.getTags() != null && journey.getTags().contains("ridesharing")) {
+                                        carpoolingJourneys.add(journey);
+                                    } else {
+                                        classicJourneys.add(journey);
+                                    }
+                                }
+
+                                showResult();
+                            } else {
+                                showError();
+                            }
+                        }
+
+                        @Override
+                        public void onUploadProgress(long bytesWritten, long contentLength, boolean done) { }
+
+                        @Override
+                        public void onDownloadProgress(long bytesRead, long contentLength, boolean done) { }
+                    });
+        } catch (Exception e) {
+            showError();
+            e.printStackTrace();
+        }
+    }
+
+    private void showError() {
+
+    }
+
+    private void showResult() {
+
     }
 }
--- navitiasdkux/src/main/java/org/kisio/navitiasdkui/util/Configuration.java
+++ navitiasdkux/src/main/java/org/kisio/navitiasdkui/util/Configuration.java
@@ -3,10 +3,14 @@ package org.kisio.navitiasdkui.util;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
+
+import org.kisio.navitiasdkui.R;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,122 +39,13 @@ public class Configuration {
         }
     }
 
-    public static class colors {
-        static Integer primary = Color.parseColor("#666666");
-        static Integer primaryText = Color.parseColor("#ffffff");
-        static Integer secondary = Color.parseColor("#f1f1f1");
-        static Integer secondaryText = Color.parseColor("#ffffff");
-        static Integer tertiary = Color.parseColor("#40958e");
-        static Integer tertiaryText = Color.parseColor("#ffffff");
-        static Integer brightText = Color.parseColor("#ffffff");
-        static Integer darkText = Color.parseColor("#000000");
-        static Integer white = Color.parseColor("#ffffff");
-        static Integer lighterGray = Color.parseColor("#f1f1f1");
-        static Integer lightGray = Color.parseColor("#cdcdcd");
-        static Integer gray = Color.parseColor("#9a9a9a");
-        static Integer darkGray = Color.parseColor("#404040");
-        static Integer darkerGray = Color.parseColor("#202020");
-        static Integer origin = Color.parseColor("#00bb75");
-        static Integer destination = Color.parseColor("#b00353");
-        static Integer transparentBlack = Color.parseColor("#bb000000");
+    public static int getColor(Context context, int wantedColor) {
+        TypedValue typedValue = new TypedValue();
+        TypedArray a = context.obtainStyledAttributes(typedValue.data, new int[] { wantedColor });
+        int color = a.getColor(0, 0);
 
-        public static Integer getPrimary() {
-            return primary;
-        }
+        a.recycle();
 
-        public static void setPrimary(Integer primary) {
-            colors.primary = primary;
-            colors.primaryText = org.kisio.navitiasdkui.util.Color.contrastColor(primary);
-        }
-
-        public static Integer getPrimaryText() {
-            return primaryText;
-        }
-
-        public static Integer getSecondary() {
-            return secondary;
-        }
-
-        public static void setSecondary(Integer secondary) {
-            colors.secondary = secondary;
-            colors.secondaryText = org.kisio.navitiasdkui.util.Color.contrastColor(secondary);
-        }
-
-        public static Integer getSecondaryText() {
-            return secondaryText;
-        }
-
-        public static Integer getTertiary() {
-            return tertiary;
-        }
-
-        public static void setTertiary(Integer tertiary) {
-            colors.tertiary = tertiary;
-            colors.tertiaryText = org.kisio.navitiasdkui.util.Color.contrastColor(tertiary);
-        }
-
-        public static Integer getTertiaryText() {
-            return tertiaryText;
-        }
-
-        public static Integer getBrightText() {
-            return brightText;
-        }
-
-        public static void setBrightText(Integer brightText) {
-            colors.brightText = brightText;
-        }
-
-        public static Integer getDarkText() {
-            return darkText;
-        }
-
-        public static void setDarkText(Integer darkText) {
-            colors.darkText = darkText;
-        }
-
-        public static Integer getWhite() {
-            return white;
-        }
-
-        public static Integer getLighterGray() {
-            return lighterGray;
-        }
-
-        public static Integer getLightGray() {
-            return lightGray;
-        }
-
-        public static Integer getGray() {
-            return gray;
-        }
-
-        public static Integer getDarkGray() {
-            return darkGray;
-        }
-
-        public static Integer getDarkerGray() {
-            return darkerGray;
-        }
-
-        public static Integer getOrigin() {
-            return origin;
-        }
-
-        public static void setOrigin(Integer origin) {
-            colors.origin = origin;
-        }
-
-        public static Integer getDestination() {
-            return destination;
-        }
-
-        public static void setDestination(Integer destination) {
-            colors.destination = destination;
-        }
-
-        public static Integer getTransparentBlack() {
-            return transparentBlack;
-        }
+        return color;
     }
 }
